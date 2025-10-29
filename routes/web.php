044d710c59d0b88be11ef90a655841e3e9f1cad3
kaushik <kaushikpaul29999@gmail.com>
2025-10-29 22:20:46 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -159,7 +159,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/search', [SearchController::class, 'index'])->name('search');
+// Route::get('/search', [SearchController::class, 'index'])->name('search');
 
 
 require __DIR__ . '/auth.php';
